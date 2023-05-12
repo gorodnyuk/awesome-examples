4da74921041d796f30099041369c57b8279e779a
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-05-12 19:10:58 +0300

    Init commit

---
+++ src/main/java/pro/gorodnyuk/awesomeexamples/AwesomeExamplesApplication.java
@@ -0,0 +1,13 @@
+package pro.gorodnyuk.awesomeexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/gorodnyuk/awesomeexamples/AwesomeExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.gorodnyuk.awesomeexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
