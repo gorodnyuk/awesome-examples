d3cc062c1253a562d5994eb681a5a08174143d6b
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-08-06 16:07:40 +0300

    Закончен тестовый сервис

---
+++ _04-tests/src/main/java/pro/gorodnyuk/service/DefaultGenerator.java
@@ -0,0 +1,12 @@
+package pro.gorodnyuk.service;
+
+import org.springframework.stereotype.Component;
+import pro.gorodnyuk.model.Person;
+
+@Component
+public class DefaultGenerator {
+
+    public Person getDefaultPerson() {
+        return new Person(999L, "Oleg", "xxx_Oleg_xxx", "Moscow");
+    }
+}
--- _04-tests/src/main/java/pro/gorodnyuk/service/PersonService.java
+++ _04-tests/src/main/java/pro/gorodnyuk/service/PersonService.java
@@ -1,20 +1,50 @@
 package pro.gorodnyuk.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import pro.gorodnyuk.model.Person;
+import pro.gorodnyuk.repository.PersonRepository;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
 
+    private final DefaultGenerator defaultGenerator;
+    private final PersonRepository personRepository;
+    private final RestTemplate restTemplate;
+
+    public PersonService(DefaultGenerator defaultGenerator, PersonRepository personRepository) {
+        this.defaultGenerator = defaultGenerator;
+        this.personRepository = personRepository;
+        this.restTemplate = new RestTemplate();
+    }
+
+    /**
+     * Взять сущность из текущего проекта
+     *
+     * @return person
+     */
     public Person getDefaultPerson() {
-        return null;
+        return defaultGenerator.getDefaultPerson();
     }
 
+    /**
+     * Взять сущность из базы данных
+     *
+     * @param nickName ник
+     * @return person
+     */
     public Person getPersonByNickName(String nickName) {
-        return null;
+        return personRepository.findByNickName(nickName).orElse(null);
     }
 
+    /**
+     * Взять сущность из интеграции с другой системой
+     *
+     * @return person
+     */
     public Person getPersonFromIntegration() {
-        return null;
+        return restTemplate.getForObject("http:localhost:9090/person", Person.class);
     }
 }
